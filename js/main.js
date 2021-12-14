80fec0d0f38ba0a97e2ea636858b8950330aac7e
Inma Sánchez Casabán <iscasaban@gmail.com>
2021-12-14 11:10:22 +0100

    evaluacion intermedia mod2

--- js/main.js
+++ js/main.js
@@ -5,15 +5,47 @@
 const updateButton = document.querySelector(".js_button");
 const selectMove = document.querySelector(".js_select_move");
 const gameResult = document.querySelector(".js_game_result");
-const arr3 = ["Piedra", "Papel", "Tijera"];
+const moves = ["Piedra", "Papel", "Tijera"];
 // 2. Funciones:
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
+function computerMove() {
+  const randomNum = getRandomNumber(10);
+  if (randomNum <= 3) {
+    return moves[0];
+  } else if (randomNum >= 6) {
+    return moves[1];
+  } else {
+    return moves[2];
+  }
+}
+
 function handleClickUpdate(event) {
   event.preventDefault();
+  const userMove = selectMove.value;
+  const compMove = computerMove();
+  if (compMove === "Tijera" && userMove === "Tijera") {
+    gameResult.innerHTML = "Empate";
+  } else if (compMove === "Piedra" && userMove === "Tijera") {
+    gameResult.innerHTML = "Gana computadora";
+  } else if (compMove === "Papel" && userMove === "Tijera") {
+    gameResult.innerHTML = "Gana usuario";
+  } else if (compMove === "Tijera" && userMove === "Piedra") {
+    gameResult.innerHTML = "Gana usuario";
+  } else if (compMove === "Piedra" && userMove === "Piedra") {
+    gameResult.innerHTML = "Empate";
+  } else if (compMove === "Papel" && userMove === "Piedra") {
+    gameResult.innerHTML = "Gana computadora";
+  } else if (compMove === "Tijera" && userMove === "Papel") {
+    gameResult.innerHTML = "Gana computadora";
+  } else if (compMove === "Piedra" && userMove === "Papel") {
+    gameResult.innerHTML = "Gana usuario";
+  } else if (compMove === "Papel" && userMove === "Papel") {
+    gameResult.innerHTML = "Empate";
+  }
 }
 
 // 3. Código que se ejecuta cuando se carga la página
