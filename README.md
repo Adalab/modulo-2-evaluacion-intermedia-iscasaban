b706490387552858c199b532f526dec6b43abf21
Inma Sánchez Casabán <iscasaban@gmail.com>
2022-01-04 18:17:00 +0100

    Add improvements after feedback: add counter variable for total rounds; check if round counter has reached the limit of rounds; remove attributes with empty values from html; disable play button when the limit rounds is reached; display button to restart the game; show info message for total rounds

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Módulo 2: Ejercicio de evaluación intermedia
+# ¡Piedra, papel o tijera!
+:rock: :page_facing_up:	:scissors:
 
 El ejercicio consiste en desarrollar el juego "Piedra, Papel y Tijera".
 
-En el juego ambos jugadores tienen que escoger piedra, papel o tijeras. Solo tiene dos resultados posibles: un empate o una victoria para un
-jugador y una derrota para el otro jugador. Diseñaremos el juego usando JavaScript donde un jugador jugará contra la computadora.
+En el juego ambos jugadores tienen que escoger piedra, papel o tijeras. Solo tiene dos resultados posibles: un empate o una victoria para un jugador y una derrota para el otro jugador. Diseñaremos el juego usando JavaScript donde un jugador jugará contra la computadora.
 
 https://beta.adalab.es/modulo-2-evaluacion-intermedia-iscasaban/
--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-"use strict";
+'use strict';
 
 // 1. Variables globales
 
-const updateButton = document.querySelector(".js_button");
-const selectMove = document.querySelector(".js_select_move");
-const gameResult = document.querySelector(".js_game_result");
-const moves = ["Piedra", "Papel", "Tijera"];
+const updateButton = document.querySelector('.js_button');
+const restartBtn = document.querySelector('.js_btn_restart');
+const selectMove = document.querySelector('.js_select_move');
+const gameResult = document.querySelector('.js_game_result');
+const gameMoves = document.querySelector('.js_game_moves');
+const moves = ['Piedra', 'Papel', 'Tijera'];
+const player = document.querySelector('.js_player');
+const computer = document.querySelector('.js_computer');
 let userCounter = 0;
 let compCounter = 0;
-const player = document.querySelector(".js_player");
-const computer = document.querySelector(".js_computer");
+let totalRounds = 0;
 
 // 2. Funciones:
 
@@ -18,10 +21,10 @@ function getRandomNumber(max) {
 }
 
 function computerMove() {
-  const randomNum = getRandomNumber(10);
+  const randomNum = getRandomNumber(11);
   if (randomNum <= 3) {
     return moves[0];
-  } else if (randomNum >= 6) {
+  } else if (randomNum >= 7) {
     return moves[1];
   } else {
     return moves[2];
@@ -32,40 +35,51 @@ function handleClickUpdate(event) {
   event.preventDefault();
   const userMove = selectMove.value;
   const compMove = computerMove();
-  if (compMove === "Tijera" && userMove === "Tijera") {
+  if (compMove === userMove) {
     tie();
-  } else if (compMove === "Piedra" && userMove === "Tijera") {
+  } else if (compMove === 'Piedra' && userMove === 'Tijera') {
     computerWins();
-  } else if (compMove === "Papel" && userMove === "Tijera") {
+  } else if (compMove === 'Papel' && userMove === 'Tijera') {
     userWins();
-  } else if (compMove === "Tijera" && userMove === "Piedra") {
+  } else if (compMove === 'Tijera' && userMove === 'Piedra') {
     userWins();
-  } else if (compMove === "Piedra" && userMove === "Piedra") {
-    tie();
-  } else if (compMove === "Papel" && userMove === "Piedra") {
+  } else if (compMove === 'Papel' && userMove === 'Piedra') {
     computerWins();
-  } else if (compMove === "Tijera" && userMove === "Papel") {
+  } else if (compMove === 'Tijera' && userMove === 'Papel') {
     computerWins();
-  } else if (compMove === "Piedra" && userMove === "Papel") {
+  } else if (compMove === 'Piedra' && userMove === 'Papel') {
     userWins();
-  } else if (compMove === "Papel" && userMove === "Papel") {
-    tie();
   }
+  totalRounds++;
+  endGame();
+  gameMoves.innerHTML = 'Número de jugadas: ' + totalRounds;
 }
 
 function tie() {
-  gameResult.innerHTML = "Empate";
+  gameResult.innerHTML = 'Empate';
 }
 function userWins() {
-  gameResult.innerHTML = "Gana usuario";
+  gameResult.innerHTML = 'Gana usuario';
   userCounter++;
-  player.innerHTML = "Jugador: " + userCounter;
+  player.innerHTML = 'Jugador: ' + userCounter;
 }
 function computerWins() {
-  gameResult.innerHTML = "Gana computadora";
+  gameResult.innerHTML = 'Gana computadora';
   compCounter++;
-  computer.innerHTML = "Computadora: " + compCounter;
+  computer.innerHTML = 'Computadora: ' + compCounter;
+}
+
+function endGame() {
+  if (totalRounds === 10) {
+    gameResult.innerHTML = '¡Juego finalizado!';
+    updateButton.classList.add('disabled__button');
+    updateButton.disabled = true;
+    restartBtn.disabled = false;
+    restartBtn.classList.remove('disabled__button');
+  }
 }
 
 // 3. Código que se ejecuta cuando se carga la página
-updateButton.addEventListener("click", handleClickUpdate);
+updateButton.addEventListener('click', handleClickUpdate);
+restartBtn.disabled = true;
+restartBtn.classList.add('disabled__button');
